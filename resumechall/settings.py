4b0e1aae97bdd047e8229502fed0d351632455ae
Blacktonystark <gregorie.thomas@gmail.com>
2024-09-30 17:24:50 +1300

    uhh lots of dumb changes

--- resumechall/settings.py
+++ resumechall/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
 #    "resumechall.apps.public",
     "resumechall.apps.accounts",
 #    "accounts",
