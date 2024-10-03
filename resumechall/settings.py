9a66e1593ce57a69fe79e0b10c135c843e06386d
Blacktonystark <gregorie.thomas@gmail.com>
2024-10-03 16:22:12 +1300

    a bunch of js and changing routing on the navbar

--- resumechall/settings.py
+++ resumechall/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Governs login/logout behavior
 LOGIN_URL = "accounts:login"
